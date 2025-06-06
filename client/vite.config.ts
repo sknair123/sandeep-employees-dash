cb6f4a2a097234b2c3f6f905a0bb2971cd581dda
Sarath K Nair <sknair143@gmail.com>
2025-06-06 16:19:13 +0530

    last port fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,
-    strictPort: true, // Fail if port is in use
-    host: true, // Listen on all network interfaces
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const frontendUrl = new URL(env.FRONTEND_URL || 'http://localhost:3001')
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(frontendUrl.port),
+      strictPort: true, // Fail if port is in use
+      host: true, // Listen on all network interfaces
+    },
+  }
 })
